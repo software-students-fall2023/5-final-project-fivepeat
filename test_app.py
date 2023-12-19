221f70e59b5871580daf53d59a54dd414683ab2f
dj9771 <yj2363@nyu.edu>
2023-12-18 21:30:37 -0500

    finalize test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,3 @@
-# tests/test_app.py
-
 import pytest
 from app import app
 from flask import session
@@ -16,147 +14,112 @@ def test_index_route(client):
     response = client.get('/')
     assert response.status_code == 200
 
-# Testing the '/login' route
 def test_login_route(client):
     response = client.get('/login')
-    assert response.status_code == 302  # Ensure proper redirection
+    assert response.status_code == 302  
 
-# Test if unauthorized access to restricted pages redirects to login
 def test_top_artists_route_unauthorized(client):
     response = client.get('/top_artists')
     assert response.status_code == 302
-    assert b'/login' in response.data  # Check if redirected to login page
+    assert b'/login' in response.data  
 
-# Test if valid access token grants access to '/top_artists' route
 def test_top_artists_route_authorized(client):
     with client.session_transaction() as sess:
         sess['access_token'] = 'valid_token_here'
-        sess['expires_at'] = 9999999999  # Future timestamp
+        sess['expires_at'] = 9999999999  
 
     response = client.get('/top_artists')
     assert response.status_code == 200
 
-# Test handling of callback with an error
 def test_callback_error_handling(client):
     response = client.get('/callback?error=some_error')
-    assert response.status_code == 200  # Check if it handles error
+    assert response.status_code == 200  
 
-# Testing the '/refresh-token' route for unauthorized access
 def test_refresh_token_route_unauthorized(client):
     response = client.get('/refresh-token')
     assert response.status_code == 302
-    assert b'/login' in response.data  # Check if redirected to login page
+    assert b'/login' in response.data  
 
-# Test if valid refresh token grants access to '/refresh-token' route
-'''def test_refresh_token_route_authorized(client):
-    with client.session_transaction() as sess:
-        sess['refresh_token'] = 'valid_refresh_token_here'
-        sess['expires_at'] = 9999999999  # Future timestamp
-
-    response = client.get('/refresh-token')
-    assert response.status_code == 302
-    assert b'/top_artists' in response.data  # Check if redirected to top_artists'''
-
-# Testing the '/quiz' route for unauthorized access
 def test_quiz_route_unauthorized(client):
     response = client.get('/quiz')
     assert response.status_code == 302
-    assert b'/login' in response.data  # Check if redirected to login page
+    assert b'/login' in response.data 
 
-# Test if valid access token grants access to '/quiz' route
-'''def test_quiz_route_authorized(client):
-    with client.session_transaction() as sess:
-        sess['access_token'] = 'valid_token_here'
-        sess['expires_at'] = 9999999999  # Future timestamp
-
-    response = client.get('/quiz')
-    assert response.status_code == 200'''
-
-# Testing the '/random_song' route for unauthorized access
 def test_random_song_route_unauthorized(client):
     response = client.get('/random_song')
     assert response.status_code == 302
-    assert b'/login' in response.data  # Check if redirected to login page
+    assert b'/login' in response.data 
 
-# Test if valid access token grants access to '/random_song' route
 def test_random_song_route_authorized(client):
     with client.session_transaction() as sess:
         sess['access_token'] = 'valid_token_here'
-        sess['expires_at'] = 9999999999  # Future timestamp
+        sess['expires_at'] = 9999999999  
 
     response = client.get('/random_song')
     assert response.status_code == 200
 
-# Testing the '/history' route for unauthorized access
 def test_history_route_unauthorized(client):
     response = client.get('/history')
     assert response.status_code == 302
-    assert b'/login' in response.data  # Check if redirected to login page
+    assert b'/login' in response.data  
 
-# Test if valid access token grants access to '/history' route
 def test_history_route_authorized(client):
     with client.session_transaction() as sess:
         sess['access_token'] = 'valid_token_here'
-        sess['expires_at'] = 9999999999  # Future timestamp
+        sess['expires_at'] = 9999999999  
 
     response = client.get('/history')
     assert response.status_code == 200
 
-# Add more tests for other routes and functionalities
-
 def test_quiz_route_redirects_when_access_token_not_in_session(client, monkeypatch):
-    # Mock session without 'access_token'
+
     with client.session_transaction() as sess:
-        sess.clear()  # Clear any existing session data
+        sess.clear()  
         
     response = client.get('/quiz')
     assert response.status_code == 302
-    assert response.location == '/login'  # Replace with your redirect URL
+    assert response.location == '/login'  
 
 def test_quiz_route_redirects_when_token_expired(client, monkeypatch):
-    # Mock session with expired token
+    
     with client.session_transaction() as sess:
         sess['access_token'] = 'valid_access_token_here'
-        sess['expires_at'] = 1000  # Set an expired timestamp
+        sess['expires_at'] = 1000  
         
     response = client.get('/quiz')
     assert response.status_code == 302
-    assert response.location == '/refresh-token'  # Replace with your redirect URL
-
-# Add more tests for other scenarios, API calls, session handling, etc.
+    assert response.location == '/refresh-token'  
 
 def test_random_song_redirects_to_login_when_no_access_token(client):
     with client.session_transaction() as sess:
-        # No access token in session
         sess.pop('access_token', None)
 
     response = client.get('/random_song')
     assert response.status_code == 302
-    assert response.location == '/login'  # Adjust the URL as per your application
+    assert response.location == '/login'  
 
 def test_random_song_redirects_to_refresh_token_when_token_expired(client):
     with client.session_transaction() as sess:
-        # Set an expired timestamp
         sess['expires_at'] = datetime.now() - timedelta(hours=1)
 
     response = client.get('/random_song')
     assert response.status_code == 302
-    assert response.location == '/login'  # Adjust the URL as per your application
+    assert response.location == '/login' 
 
 def test_callback_with_error_in_request_args(client):
-    # Simulate a callback with an error in request.args
+    
     response = client.get('/callback?error=test_error')
     assert response.status_code == 200
     assert response.json == {"error": "test_error"}
 
 @patch('app.requests.post')
 def test_callback_with_code_in_request_args(mock_post, client):
-    # Simulate a callback with a code in request.args
+    
     mock_response = mock_post.return_value
     mock_response.json.return_value = {
         'access_token': 'test_access_token',
         'refresh_token': 'test_refresh_token',
-        'expires_in': 3600  # Assuming the expires_in value
+        'expires_in': 3600  
     }
 
     response = client.get('/callback?code=test_code')
@@ -165,48 +128,38 @@ def test_callback_with_code_in_request_args(mock_post, client):
     assert session['refresh_token'] == 'test_refresh_token'
     assert 'expires_at' in session
 
-    # Add assertions for expires_at based on the expected value,
-    # for example, compare with current timestamp + expires_in
-    
-    # Example (using assert for expires_at):
-    expected_expires_at = datetime.now().timestamp() + 3600  # Expected expires_at value
-    assert abs(session['expires_at'] - expected_expires_at) < 1  # Tolerance level of 1 second
+    expected_expires_at = datetime.now().timestamp() + 3600  
+    assert abs(session['expires_at'] - expected_expires_at) < 1  
 
     assert response.location == '/top_artists'
 
 @patch('app.requests.post')
 def test_refresh_token_route(mock_post, client):
-    # Set up session data
+    
     with client.session_transaction() as sess:
         sess['refresh_token'] = 'valid_refresh_token_here'
-        sess['expires_at'] = datetime.now().timestamp() + 3600  # Expires in an hour
+        sess['expires_at'] = datetime.now().timestamp() + 3600  
 
-    # Mocked response from the external API
     mock_response = mock_post.return_value
     mock_response.json.return_value = {
         'access_token': 'new_access_token',
-        'expires_in': 3600  # Assuming the expires_in value
+        'expires_in': 3600  
     }
 
-    # Perform the request to the /refresh-token route
     response = client.get('/refresh-token')
 
-    # Assertions
     assert response.status_code == 302
     assert session['access_token'] == 'new_access_token'
     assert 'expires_at' in session
 
-    # Calculate the expected expiration time
-    expected_expires_at = datetime.now().timestamp() + 3600  # Expected expires_at value
-    assert abs(session['expires_at'] - expected_expires_at) < 1  # Tolerance level of 1 second
+    expected_expires_at = datetime.now().timestamp() + 3600  
+    assert abs(session['expires_at'] - expected_expires_at) < 1  
 
-
-    # Check the redirected URL
     assert response.location == '/top_artists'
 
 @patch('app.db.collection.insert_one')
 def test_quiz_submit_route(mock_insert_one, client):
-    # Set up session data
+    
     with client.session_transaction() as sess:
         sess['correct_answer'] = 'correct_answer_id'
         sess['quiz_data'] = {
@@ -214,17 +167,13 @@ def test_quiz_submit_route(mock_insert_one, client):
             'song2': {'name': 'Song 2', 'features2': 'Feature 2'},
             'song1_image_url': 'image_url_1',
             'song2_image_url': 'image_url_2'
-            # Add other required data here
         }
 
-    # Simulate a POST request with the form data containing the correct answer
     response = client.post('/quiz/submit', data={'answer1': 'correct_answer_id'})
 
-    # Assertions
     assert response.status_code == 200
-    assert b'Correct!' in response.data  # Assuming the template renders 'Correct!' for a correct answer
+    assert b'Correct!' in response.data  #
 
-    # Check if the database insert function was called with the expected data
     mock_insert_one.assert_called_once_with({
         "Song 1 Name": 'Song 1',
         "Song 1 Features": 'Feature 1',
@@ -238,7 +187,7 @@ def test_quiz_submit_route(mock_insert_one, client):
 
 @patch('app.db.collection.insert_one')
 def test_quiz_submit_route(mock_insert_one, client):
-    # Set up session data with required keys excluding 'features1'
+    
     with client.session_transaction() as sess:
         sess['correct_answer'] = 'correct_answer_id'
         sess['quiz_data'] = {
@@ -246,45 +195,38 @@ def test_quiz_submit_route(mock_insert_one, client):
             'song2': {'name': 'Song 2'},
             'song1_image_url': 'image_url_1',
             'song2_image_url': 'image_url_2'
-            # Add other required data here
         }
 
-    # Simulate a POST request with the form data containing the correct answer
     request_data = {'answer1': 'correct_answer_id'}
     response = client.post('/quiz/submit', data=request_data)
-
-    # Assertions
+   
     assert response.status_code == 200
-    assert b'Correct!' in response.data  # Assuming the template renders 'Correct!' for a correct answer
+    assert b'Correct!' in response.data  
     mock_insert_one.assert_called_once()
-
 
 @pytest.fixture
 def mock_features_response():
-    # Define and return your mocked features response here
     pass
 
 @pytest.fixture
 def mock_tracks_response():
-    # Define and return your mocked tracks response here
     pass
 
 def test_quiz_route_redirects_to_login_when_no_access_token(mock_features_response, mock_tracks_response, client):
-    # Mock 'access_token' not in session
+    
     with client.session_transaction() as sess:
-        sess.clear()  # Clear any existing session data
+        sess.clear()  
     
     response = client.get('/quiz')
     assert response.status_code == 302
     assert response.location == '/login'
 
-
 @patch('app.requests.get')
 @patch('app.requests.get')
 def test_top_artists_redirects_to_login_when_no_access_token(mock_response_1, mock_response_2, client):
-    # Mock 'access_token' not in session
+   
     with client.session_transaction() as sess:
-        sess.clear()  # Clear any existing session data
+        sess.clear()  
     
     response = client.get('/top_artists')
     assert response.status_code == 302
@@ -297,40 +239,10 @@ def test_top_artists_redirects_to_refresh_token_when_expired(mock_response_1, mo
     with client.session_transaction() as sess:
         sess['access_token'] = 'test_access_token'
         sess['expires_at'] = (datetime.now() - timedelta(hours=1)).timestamp()
-        # Add any other required data to the session
     
     response = client.get('/top_artists')
     assert response.status_code == 302
     assert response.location == '/refresh-token'
-
-'''@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-
-@patch('app.requests.get')
-@patch('app.requests.get')
-def test_quiz_route(mock_features_response, mock_tracks_response, client):
-    with client.session_transaction() as sess:
-        sess['access_token'] = 'test_access_token'
-        sess['expires_at'] = (datetime.now() + timedelta(hours=1)).timestamp()
-
-    # Mock API responses for saved tracks and audio features
-    mock_tracks_response.return_value.json.return_value = {
-        'items': [
-            {'id': 'song1_id', 'name': 'Song 1'},
-            {'id': 'song2_id', 'name': 'Song 2'}
-        ]
-    }
-    mock_features_response.return_value.json.return_value = {
-        # Simulating an empty response or response without 'audio_features'
-        # Adjust as needed based on your requirements for testing this behavior
-    }
-
-    response = client.get('/quiz')
-    assert response.status_code == 302  # Assuming it redirects to '/refresh-token' due to missing 'audio_features'''
-
 
 @pytest.fixture
 def client():
@@ -351,10 +263,8 @@ def test_random_song_route(mock_get, client):
         MagicMock(json=lambda: {'tracks': {'items': [{'track': {'name': 'Track Name'}}]}})
     ]
 
-    # Run the random_song route
     response = client.get('/random_song')
 
-    # Perform assertions
     assert b'Track Name' in response.data
 
 @patch('app.requests.get')
@@ -393,7 +303,6 @@ def test_random_song_route(mock_get, client):
     # Perform assertions
     assert b'Track Name' not in response.data
     assert b'No tracks found in the selected playlist' in response.data
-    # Add more assertions based on expected data in the response
 
 def test_app_exists():
     assert app is not None
@@ -401,19 +310,14 @@ def test_app_exists():
 def test_app_debug_is_true():
     assert app.debug is False
 
-
-from unittest.mock import patch
-from app import app  # Assuming your Flask app instance is called 'app'
-
 @patch('app.requests.get')
 def test_top_artists_route(mock_requests_get):
-    # Mock session data
+   
     with app.test_client() as client:
         with client.session_transaction() as session:
             session['access_token'] = 'test_access_token'
-            session['expires_at'] = 1893456000  # Some timestamp in the future
+            session['expires_at'] = 1893456000 
     
-        # Mock the response from the external API
         mock_requests_get.return_value.json.return_value = {
             'items': [
                 {
@@ -427,10 +331,8 @@ def test_top_artists_route(mock_requests_get):
             ]
         }
         
-        # Make a GET request to the route
         response = client.get('/top_artists')
         
-        # Assertions
         assert response.status_code == 200
         assert b'Artist 1' in response.data
         assert b'Artist 2' in response.data
