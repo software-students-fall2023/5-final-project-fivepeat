6b2267bf6cc4049a356d20ff1f1366ce9a3de08d
dj9771 <yj2363@nyu.edu>
2023-12-18 21:17:57 -0500

    test for quiz submit

--- test_app.py
+++ test_app.py
@@ -148,3 +148,90 @@ def test_callback_with_error_in_request_args(client):
     response = client.get('/callback?error=test_error')
     assert response.status_code == 200
     assert response.json == {"error": "test_error"}
+
+@patch('app.requests.post')
+def test_callback_with_code_in_request_args(mock_post, client):
+    # Simulate a callback with a code in request.args
+    mock_response = mock_post.return_value
+    mock_response.json.return_value = {
+        'access_token': 'test_access_token',
+        'refresh_token': 'test_refresh_token',
+        'expires_in': 3600  # Assuming the expires_in value
+    }
+
+    response = client.get('/callback?code=test_code')
+    assert response.status_code == 302
+    assert session['access_token'] == 'test_access_token'
+    assert session['refresh_token'] == 'test_refresh_token'
+    assert 'expires_at' in session
+
+    # Add assertions for expires_at based on the expected value,
+    # for example, compare with current timestamp + expires_in
+    
+    # Example (using assert for expires_at):
+    expected_expires_at = datetime.now().timestamp() + 3600  # Expected expires_at value
+    assert abs(session['expires_at'] - expected_expires_at) < 1  # Tolerance level of 1 second
+
+    assert response.location == '/top_artists'
+
+@patch('app.requests.post')
+def test_refresh_token_route(mock_post, client):
+    # Set up session data
+    with client.session_transaction() as sess:
+        sess['refresh_token'] = 'valid_refresh_token_here'
+        sess['expires_at'] = datetime.now().timestamp() + 3600  # Expires in an hour
+
+    # Mocked response from the external API
+    mock_response = mock_post.return_value
+    mock_response.json.return_value = {
+        'access_token': 'new_access_token',
+        'expires_in': 3600  # Assuming the expires_in value
+    }
+
+    # Perform the request to the /refresh-token route
+    response = client.get('/refresh-token')
+
+    # Assertions
+    assert response.status_code == 302
+    assert session['access_token'] == 'new_access_token'
+    assert 'expires_at' in session
+
+    # Calculate the expected expiration time
+    expected_expires_at = datetime.now().timestamp() + 3600  # Expected expires_at value
+    assert abs(session['expires_at'] - expected_expires_at) < 1  # Tolerance level of 1 second
+
+
+    # Check the redirected URL
+    assert response.location == '/top_artists'
+
+@patch('app.db.collection.insert_one')
+def test_quiz_submit_route(mock_insert_one, client):
+    # Set up session data
+    with client.session_transaction() as sess:
+        sess['correct_answer'] = 'correct_answer_id'
+        sess['quiz_data'] = {
+            'song1': {'name': 'Song 1', 'features1': 'Feature 1'},
+            'song2': {'name': 'Song 2', 'features2': 'Feature 2'},
+            'song1_image_url': 'image_url_1',
+            'song2_image_url': 'image_url_2'
+            # Add other required data here
+        }
+
+    # Simulate a POST request with the form data containing the correct answer
+    response = client.post('/quiz/submit', data={'answer1': 'correct_answer_id'})
+
+    # Assertions
+    assert response.status_code == 200
+    assert b'Correct!' in response.data  # Assuming the template renders 'Correct!' for a correct answer
+
+    # Check if the database insert function was called with the expected data
+    mock_insert_one.assert_called_once_with({
+        "Song 1 Name": 'Song 1',
+        "Song 1 Features": 'Feature 1',
+        "Song 2 Name": 'Song 2',
+        "Song 2 Features": 'Feature 2',
+        "Song 1 Image URL": 'image_url_1',
+        "Song 2 Image URL": 'image_url_2',
+        "Result": 'Correct!',
+        "timestamp_field": datetime.utcnow()
+    })
