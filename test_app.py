f741734520b25161ac9461a8212d0123f3005e50
dj9771 <yj2363@nyu.edu>
2023-12-18 21:17:09 -0500

    Test for random song

--- test_app.py
+++ test_app.py
@@ -71,3 +71,80 @@ def test_quiz_route_unauthorized(client):
 
     response = client.get('/quiz')
     assert response.status_code == 200'''
+
+# Testing the '/random_song' route for unauthorized access
+def test_random_song_route_unauthorized(client):
+    response = client.get('/random_song')
+    assert response.status_code == 302
+    assert b'/login' in response.data  # Check if redirected to login page
+
+# Test if valid access token grants access to '/random_song' route
+def test_random_song_route_authorized(client):
+    with client.session_transaction() as sess:
+        sess['access_token'] = 'valid_token_here'
+        sess['expires_at'] = 9999999999  # Future timestamp
+
+    response = client.get('/random_song')
+    assert response.status_code == 200
+
+# Testing the '/history' route for unauthorized access
+def test_history_route_unauthorized(client):
+    response = client.get('/history')
+    assert response.status_code == 302
+    assert b'/login' in response.data  # Check if redirected to login page
+
+# Test if valid access token grants access to '/history' route
+def test_history_route_authorized(client):
+    with client.session_transaction() as sess:
+        sess['access_token'] = 'valid_token_here'
+        sess['expires_at'] = 9999999999  # Future timestamp
+
+    response = client.get('/history')
+    assert response.status_code == 200
+
+# Add more tests for other routes and functionalities
+
+def test_quiz_route_redirects_when_access_token_not_in_session(client, monkeypatch):
+    # Mock session without 'access_token'
+    with client.session_transaction() as sess:
+        sess.clear()  # Clear any existing session data
+        
+    response = client.get('/quiz')
+    assert response.status_code == 302
+    assert response.location == '/login'  # Replace with your redirect URL
+
+def test_quiz_route_redirects_when_token_expired(client, monkeypatch):
+    # Mock session with expired token
+    with client.session_transaction() as sess:
+        sess['access_token'] = 'valid_access_token_here'
+        sess['expires_at'] = 1000  # Set an expired timestamp
+        
+    response = client.get('/quiz')
+    assert response.status_code == 302
+    assert response.location == '/refresh-token'  # Replace with your redirect URL
+
+# Add more tests for other scenarios, API calls, session handling, etc.
+
+def test_random_song_redirects_to_login_when_no_access_token(client):
+    with client.session_transaction() as sess:
+        # No access token in session
+        sess.pop('access_token', None)
+
+    response = client.get('/random_song')
+    assert response.status_code == 302
+    assert response.location == '/login'  # Adjust the URL as per your application
+
+def test_random_song_redirects_to_refresh_token_when_token_expired(client):
+    with client.session_transaction() as sess:
+        # Set an expired timestamp
+        sess['expires_at'] = datetime.now() - timedelta(hours=1)
+
+    response = client.get('/random_song')
+    assert response.status_code == 302
+    assert response.location == '/login'  # Adjust the URL as per your application
+
+def test_callback_with_error_in_request_args(client):
+    # Simulate a callback with an error in request.args
+    response = client.get('/callback?error=test_error')
+    assert response.status_code == 200
+    assert response.json == {"error": "test_error"}
