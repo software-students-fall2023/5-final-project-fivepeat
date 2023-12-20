2433766d4540f10065eaae44b5096e3c1722733c
gmp9469 <83371057+gmp9469@users.noreply.github.com>
2023-12-19 20:37:24 -0500

    digitalocean mongodb compatibility

--- db.py
+++ db.py
@@ -2,9 +2,20 @@
 import os
 from pymongo import MongoClient
 
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+"""DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
 CONNECTION_STRING = f"mongodb://{DATABASE_HOST}:27017/"
 
 client = MongoClient(CONNECTION_STRING)
 db = client['quiz_game_db']
+collection = db['quiz_scores']"""
+
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "doadmin")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "Si7Hu4p9ln31F026")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "mongodb+srv://db-mongodb-nyc3-33511-709f7b62.mongo.ondigitalocean.com")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "admin")
+
+CONNECTION_STRING = f"mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+client = MongoClient(CONNECTION_STRING)
+db = client[DATABASE_NAME]
 collection = db['quiz_scores']
