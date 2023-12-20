1751bb95f136ee4a5b015b2156095395dec464f7
gmp9469 <83371057+gmp9469@users.noreply.github.com>
2023-12-19 20:52:17 -0500

    Update db.py

--- db.py
+++ db.py
@@ -9,13 +9,15 @@ client = MongoClient(CONNECTION_STRING)
 db = client['quiz_game_db']
 collection = db['quiz_scores']"""
 
-DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "doadmin")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "Si7Hu4p9ln31F026")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "mongodb+srv://db-mongodb-nyc3-33511-709f7b62.mongo.ondigitalocean.com")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "admin")
 
-CONNECTION_STRING = f"mongodb+srv://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+username = "doadmin"
+password = "Si7Hu4p9ln31F026"
+host = "db-mongodb-nyc3-33511-709f7b62.mongo.ondigitalocean.com"
+database_name = "admin"
 
-client = MongoClient(CONNECTION_STRING)
-db = client[DATABASE_NAME]
+uri = f"mongodb+srv://{username}:{password}@{host}/{database_name}?retryWrites=true&w=majority"
+client = MongoClient(uri)
+db = client['quiz_game_db']
 collection = db['quiz_scores']
+
+
